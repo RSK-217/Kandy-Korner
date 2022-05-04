13382767aa09c27858def53567fdb66b6679c11e
Rick Kersmarki <rkersmarki@gmail.com>
2022-05-03 21:42:55 -0500

    database and locations

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Locations } from "./Locations/Locations.js"
+
+export const KandyKorner = () => {
+  return (
+    <>
+    <h1>Kandy Korner</h1>
+
+    <h3>Locations</h3>
+    <Locations />
+    </>
+  );
+}
+
+export default KandyKorner;
---
+++ src/components/Locations/Locations.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState} from "react"
+
+export const Locations = () => {
+    const [locations, setLocation] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/locations")
+            .then(res => res.json())
+            .then((locale) => {
+                setLocation(locale)
+            })},
+        []
+    )
+
+    return (
+        <>
+        {locations.map(
+            (location) => {
+                return <p key={`location--${location.id}`}>{location.address}</p>
+            }
+        )}
+        </>
+    )
+}
